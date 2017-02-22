347e85811dafc771ad04312a6908ff3e7e4685f2
Charlotte Dann <ask@pouretrebelle.com>
2017-02-22 21:32:40 +0000

    :sparkles: Add button and LED interaction

--- magsensor/magsensor.ino
+++ magsensor/magsensor.ino
@@ -11,14 +11,49 @@ StaticJsonBuffer<200> jsonBuffer;
 JsonObject& root = jsonBuffer.createObject();
 JsonObject& magnetic = root.createNestedObject("magnetic");
 JsonObject& acceleration = root.createNestedObject("acceleration");
+JsonObject& status = jsonBuffer.createObject();
+
+bool active = false;
+bool switchState = false;
+int LED = 6;
+int BUTTON = 5;
 
 void setup(void) {
   Serial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
   mag.enableAutoRange(true);
+
+  if(!mag.begin()) {
+    while(1);
+  }
 }
 
 void loop(void) {
+
+  if (switchState != digitalRead(BUTTON)) {
+    switchState = digitalRead(BUTTON);
+    if (switchState == true) {
+      active = !active;
+
+      if (active == true) {
+        digitalWrite(LED, HIGH);
+        status["active"] = true;
+      }
+      else {
+        digitalWrite(LED, LOW);
+        status["active"] = false;
+      }
+
+      status.printTo(Serial);
+      Serial.println();
+    }
+  }
+
   sensors_event_t event;
+  mag.getEvent(&event);
 
   magnetic["x"] = event.magnetic.x;
   magnetic["y"] = event.magnetic.y;
@@ -27,9 +62,9 @@ void loop(void) {
   acceleration["x"] = event.acceleration.x;
   acceleration["y"] = event.acceleration.y;
   acceleration["z"] = event.acceleration.z;
-  
+
   root.printTo(Serial);
   Serial.println();
-  
+
   delay(50);
 }
